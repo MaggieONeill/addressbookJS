b220ace839d6a89399795c7a3b4922b979e90fab
Maggie O'Neill <maggieoneill@Maggie-ONeill.local>
2015-09-21 15:57:57 -0700

    fixed the highlight and show contacts bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-var
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedAddress = $("input#new-address").val();
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input#address").val("");
+
+  $(".contact").last().click(function() {//.last only binds the event to the most recently added contact... otherwise it would show in all contacts the info you just added.
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+    $(".firstName").text(newContact.firstName);
+    $(".lastName").text(newContact.lastName);
+    $(".address").text(newContact.address);
+  });
+  });
+});
